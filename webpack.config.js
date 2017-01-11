ebf9c3b22836b845a0e847205d016c19b331019e
renhongl <1075220132@qq.com>
2017-01-11 12:56:39 +0800

    Added base structure

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+module.exports = {
+    entry: path.resolve(__dirname, 'app/main.js'),
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js',
+    },
+    module: {
+        loaders: [{
+            test: /\.js?$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+            query: {
+                presets: ['es2015']
+            }
+        }, {
+            test: /\.css$/,
+            loader: 'style!css'
+        }]
+    }
+};
